6b957e67f6b75130388a14199c769e29e00b5235
Polina <48136939+IpolinaI@users.noreply.github.com>
2022-11-27 22:52:18 +0100

    Delete build directory

--- build/resources/main/db/migration/V1__transaction_operation_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS transaction_operation
-(
-    id             UUID             NOT NULL PRIMARY KEY,
-    account_id     UUID             NOT NULL,
-    operation_type VARCHAR(10)      NOT NULL,
-    amount         DOUBLE PRECISION NOT NULL
-);
